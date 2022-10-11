4f71e7c7126937d521ffe96c2f10396accbf5fe6
Ubuntu <ubuntu@ip-172-31-27-187.ec2.internal>
2022-10-11 06:05:08 +0000

    ALB Host Header Based Routing

---
+++ 11-ALB-Host-Header-Based-Routing/terraform-manifests/c7-03-ec2instance-bastion.tf
@@ -0,0 +1,25 @@
+# AWS EC2 Instance Terraform Module
+
+# Bastion Host - EC2 Instance that will be created in VPC Publc Subnet
+
+
+module "ec2_public" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.17.0"
+  # insert the 10 required variables here
+
+
+  name = "${var.environment}-BastionHost"
+  #   instance_count         = var.instance_count
+
+  ami                    = data.aws_ami.amzlinux2.id
+  instance_type          = var.instance_type
+  key_name               = var.instance_keypair
+  monitoring             = false
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [module.public_bastion_sg.this_security_group_id]
+
+
+  tags = local.common_tags
+}
+
