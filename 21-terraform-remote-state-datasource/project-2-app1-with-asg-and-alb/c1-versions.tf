5de9a1607db628a1df7a328f4235d4c04ec73a0a
Ubuntu <ubuntu@ip-172-31-27-187.ec2.internal>
2022-10-17 16:46:33 +0000

    Terraform on EC2

---
+++ 21-terraform-remote-state-datasource/project-2-app1-with-asg-and-alb/c1-versions.tf
@@ -0,0 +1,41 @@
+# Terraform Block
+terraform {
+  required_version = "~> 1.3.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-on-aws-for-ec2-75036347"
+    key    = "dev/project2-vpc/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "dev-project2-vpc"
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
+/*
+Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
+$HOME/.aws/credentials
+*/
+
+# Create Random Pet Resource
+resource "random_pet" "this" {
+  length = 2
+}
